cbc50d1bba97009904cced241ff46c203110c407
Mark Sargent <sargent.mark@gmail.com>
2015-05-16 18:24:55 +1200

    Added a one wire temperature sensor

--- dvs.ino
+++ dvs.ino
@@ -1,3 +1,6 @@
+#include <OneWire.h>
+#include "U8glib.h"
+
 // io
 const int temperaturePin = 0; // analog a0
 const int buttonDownPin = 11; //
@@ -8,26 +11,30 @@ const int fanSpeenPin   = 9;  //
 const int TURNONTEMP = 0;
 const int FANSPEED = 1;
 int editMode = 0;              // 0 = temp, 1 = fan speed
-int setting[]    = {19, 40};   // inital temp, inital fan speed
+int setting[]    = {19, 50};   // inital temp, inital fan speed
 int settingMin[] = {10, 0};
-int settingMax[] = {30, 240};
-int settingInc[] = {1, 20};
+int settingMax[] = {30, 250};
+int settingInc[] = {1, 25};
 
-int last_buttonUp_val = HIGH;
-int last_buttonDown_val = HIGH;
+int last_buttonUp_val = LOW;
+int last_buttonDown_val = LOW;
 
 // temperature
-int currentTemp = 10;
 int lastTemp = 0;
 int lastTempCount = 0;
 const int TEMPDATAPOINTS = 10;
 int recentTemp[TEMPDATAPOINTS];
 int nextTempRecordIndex = 0;
 
+byte room_addr[] = {0x28, 0xC8, 0x39, 0x1, 0x7, 0x0, 0x0, 0xA2};
+OneWire  room(2);
+byte ceiling_addr[8];
+OneWire  ceiling(3);
+
 // history
 const int DATAPOINTS = 96;
-const unsigned long INTERVAL = 900000;//(24 * 60 * 60 * 1000) / DATAPOINTS; // five minute intervals 
-//const unsigned long INTERVAL = 100; // testing 
+const unsigned long INTERVAL = 900000;//(24 * 60 * 60 * 1000) / DATAPOINTS; // five minute intervals
+//const unsigned long INTERVAL = 100; // testing
 int histTemp[DATAPOINTS];
 unsigned long nextRecordMilli = 0;
 unsigned int nextRecordIndex = 0;
@@ -38,8 +45,7 @@ int temp_high = -40;
 int currentFanSpeed = 0;
 
 // gfx
-#include "U8glib.h"
-U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NONE);	// I2C / TWI 
+U8GLIB_SH1106_128X64 u8g(U8G_I2C_OPT_NONE);	// I2C / TWI
 
 const  int max_size = 76;
 
@@ -67,61 +73,46 @@ void setFont(int font){
       u8g.setFont(u8g_font_5x8);
       break;
     case FONT_BIG:
-      u8g.setFont(u8g_font_helvB24);
+      u8g.setFont(u8g_font_fub25n);
       break;
   }
   u8g.setFontRefHeightExtendedText();
   u8g.setFontPosTop();
 }
 
-void drawStatus() {
+void drawStatus2(int roomTemp) {
   u8g_prepare();
 
-  
-  setFont(FONT_NORMAL);  
-  u8g.drawStr( 0, 10, ("current temp: " + String(currentTemp)).c_str()); 
-  u8g.drawStr( 0, 40,  ("fan curr speed: " + String(currentFanSpeed)).c_str()); 
-  
-  setFont(editMode == 0 ? FONT_BOLD : FONT_NORMAL);
-  u8g.drawStr( 0, 20,  ("fan temp: " + String(setting[0])).c_str());
-  
-  setFont(editMode == 1 ? FONT_BOLD : FONT_NORMAL);
-  u8g.drawStr( 0, 30,  ("fan speed: " + String(setting[1])).c_str());
-}
-
-void drawStatus2() {
-  u8g_prepare();
-  
   u8g.drawLine(44, 0, 24, 63);
   u8g.drawLine(46, 0, 26, 63);
 
   setFont(FONT_BIG);
-  int width = u8g.getStrWidth(String(currentTemp).c_str());
-  u8g.drawStr( 36 - width, 0, String(currentTemp).c_str());
-  
+  int width = u8g.getStrWidth(String(roomTemp).c_str());
+  u8g.drawStr( 36 - width, 0, String(roomTemp).c_str());
+
   setFont(FONT_SMALL);
   u8g.drawStr( 0, 50, ("L " + String(temp_low)).c_str());
   u8g.drawStr( 0, 40, ("H " + String(temp_high)).c_str());
   u8g.drawStr( 37, -1, "o");
-  
+
   setFont(FONT_NORMAL);
   u8g.drawStr(50,  0, "Fan:");
   u8g.drawStr(50, 10, "Temp:");
 
   u8g.drawStr(90,  0, currentFanSpeed > 10 ? "on" : "off");
-  
+
   // fan temp
   setFont(editMode == 0 ? FONT_BOLD : FONT_NORMAL);
   u8g.drawStr(90, 10, String(setting[0]).c_str());
-  
-  // fan speed  
+
+  // fan speed
   int speed_percentage = int((float(setting[1]) / float(settingMax[1])) * 100);
   int screen_size = (float(speed_percentage) / float(100)) * max_size;
-  
+
   if (editMode == 1)
     u8g.drawFrame(48, 21, max_size + 4, 8);
   u8g.drawBox(50, 23, screen_size, 4);
-  
+
   // print history
   for(int i = nextRecordIndex; i < DATAPOINTS + nextRecordIndex; i++){
     u8g.drawPixel(30 + i - nextRecordIndex, 60 - histTemp[i % DATAPOINTS ]);
@@ -130,168 +121,163 @@ void drawStatus2() {
 
 void setup() {
 
-  //Serial.begin(9600);
+  Serial.begin(9600);
+  
+   // 28 C8 39 1 7 0 0 A2
   
   pinMode(buttonUpPin, INPUT);
   pinMode(buttonDownPin, INPUT);
   pinMode(fanSpeenPin, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  
+
   // gfx
-  pinMode(13, OUTPUT);           
-  digitalWrite(13, HIGH); 
-  
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+
   u8g.setRot180();
 }
 
 void checkButtons(){
-  
+
   int buttonUp_val = digitalRead(buttonUpPin);
   int buttonDown_val = digitalRead(buttonDownPin);
-  
-  Serial.println("");
-  Serial.print("btn: ");
-  Serial.print(buttonUp_val);
-  Serial.println(buttonDown_val);
-  
+
   if (last_buttonUp_val != buttonUp_val)
   {
     last_buttonUp_val = buttonUp_val;
     return;
   }
-  
+
   if (last_buttonDown_val != buttonDown_val)
   {
     last_buttonDown_val = buttonDown_val;
     return;
   }
-  
-  if (buttonUp_val == LOW && buttonDown_val == LOW){
+
+  if (buttonUp_val == HIGH && buttonDown_val == HIGH){
     editMode = !editMode;
-    
-    last_buttonUp_val = HIGH;
-    last_buttonDown_val = HIGH;
+
+    last_buttonUp_val = LOW;
+    last_buttonDown_val = LOW;
   }
-  else if (buttonUp_val == LOW && setting[editMode] < settingMax[editMode]){
+  else if (buttonUp_val == HIGH && setting[editMode] < settingMax[editMode]){
     setting[editMode] = setting[editMode] + settingInc[editMode];
   }
-  else if (buttonDown_val == LOW && setting[editMode] > settingMin[editMode]){
+  else if (buttonDown_val == HIGH && setting[editMode] > settingMin[editMode]){
     setting[editMode] = setting[editMode] - settingInc[editMode];
   }
 }
 
-void printHistory(){
-  for(int j = 0; j < DATAPOINTS; j++){
-      Serial.print(histTemp[j]);
-      Serial.print("|");
-    }
-  Serial.println("");
-  Serial.print("current: ");
-  Serial.println(millis());
-  Serial.print("internval: ");
-  Serial.println(INTERVAL);
-  Serial.print("next: ");
-  Serial.println(nextRecordMilli); 
-}
 
 void recordHistory(int temp){
-  
-//  currentTemp = temp = (millis() % 30) - 5;  //fake data
-  
+
   if (nextRecordMilli < millis()){
     histTemp[nextRecordIndex] = temp;
     nextRecordIndex ++;
-    
+
     if (nextRecordIndex == DATAPOINTS)
       nextRecordIndex = 0;
-    
+
     nextRecordMilli = millis() + INTERVAL;
-    
+
     // recalculate lows and highs
     for(int i = 0; i < DATAPOINTS; i++){
       if (histTemp[i] < temp_low)
         temp_low = histTemp[i];
-      
+
       if (histTemp[i] > temp_high)
         temp_high = histTemp[i];
     }
-        
-    //printHistory();
   }
 }
 
-
-int getTemperature(){
-  
-  int rawTemp = analogRead(temperaturePin);
-  int temp = (125*rawTemp)>>8 ; // Temperature calculation formula
-  
-  recentTemp[nextTempRecordIndex] = temp;
-  
-  nextTempRecordIndex++;
-  if (nextTempRecordIndex == TEMPDATAPOINTS)
-      nextTempRecordIndex = 0;
-  
-  float total = 0;
-  for(int j = 0; j < TEMPDATAPOINTS; j++)
-      total+= recentTemp[j];
-      
-  currentTemp = int(total / TEMPDATAPOINTS);
-    
-  return currentTemp;
+void prepareTemperatureDS(OneWire ds, byte *addr){
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);
 }
 
+void prepareAndFindTemperatureDS(OneWire ds, byte *addr){
+  if ( !ds.search(addr)) {
+    ds.reset_search();
+    return;
+  }
+  
+  int i;
+  Serial.print("ROM =");
+  for( i = 0; i < 8; i++) {
+    Serial.write(' ');
+    Serial.print(addr[i], HEX);
+  }
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);
+}
+
+int getTemperatureDS(OneWire ds, byte *addr){
+
+  byte i;
+  byte data[12];
+
+  ds.reset();
+  ds.select(addr);
+  ds.write(0xBE);         // Read Scratchpad
+
+  for ( i = 0; i < 9; i++) { // we need 9 bytes
+    data[i] = ds.read();
+  }
+
+  // Convert the data to actual temperature
+  // because the result is a 16 bit signed integer, it should
+  // be stored to an "int16_t" type, which is always 16 bits
+  // even when compiled on a 32 bit processor.
+  int16_t raw = (data[1] << 8) | data[0];
+  byte cfg = (data[4] & 0x60);
+  // at lower res, the low bits are undefined, so let's zero them
+  if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+  else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+  else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+  return int((float)raw / 16.0);
+}
+
+
 void updateFan(int temp){
-  
+
   int desiredFanSpeed = currentFanSpeed;
-  
+
   if (temp > setting[TURNONTEMP])
     desiredFanSpeed = setting[FANSPEED];
   else
     desiredFanSpeed = 0;
-  
-  if (currentFanSpeed != desiredFanSpeed){
-    Serial.print("Changing fan speed to : ");
-    Serial.println(desiredFanSpeed); 
 
+  if (currentFanSpeed != desiredFanSpeed){
     analogWrite(fanSpeenPin, desiredFanSpeed);
     currentFanSpeed = desiredFanSpeed;
   }
-  
-  if (currentFanSpeed > 10)
-        digitalWrite(LED_BUILTIN, HIGH);
-}
 
-void printStatus(){
-  Serial.print("temp : "); 
-  Serial.print(currentTemp); 
-  Serial.print(" mode: "); 
-  Serial.print(editMode);
-  Serial.print(" temp: "); 
-  Serial.print(setting[0]);
-  Serial.print(" fan: "); 
-  Serial.print(setting[1]);
-  Serial.print(" fan speed: "); 
-  Serial.print(currentFanSpeed);
-  Serial.println("");
+  if (currentFanSpeed > 10)
+    digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void loop() {
- 
-  int temp = getTemperature();
- 
+  
+  prepareTemperatureDS(room, room_addr);
+  prepareTemperatureDS(ceiling, ceiling_addr);
+  
+  delay(1000);     // maybe 750ms is enough, maybe not
+  
+  int temp = getTemperatureDS(room, room_addr);
+  int temp2 = getTemperatureDS(ceiling, ceiling_addr);
+
   checkButtons();
- 
+
   updateFan(temp);
-  
+
   recordHistory(temp);
-  
-//  printStatus();
-  
-  u8g.firstPage(); 
+
+  u8g.firstPage();
   do {
-    drawStatus2();
-  } while(u8g.nextPage()); 
- 
-  delay(200);
+    drawStatus2(temp);
+  } while(u8g.nextPage());
 }
